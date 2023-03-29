49585ac7662380a51024614c817a9347e074e817
bilal-atiq <127108568+bilal-atiq@users.noreply.github.com>
2023-03-29 13:14:22 +0500

    Update streamlit_app.py
    Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit
 streamlit.title('my parents new healthy diner')
 
 streamlit.header('breakfast menu')
-streamlit.text('omega 3 and blueberry oatmeal')
-streamlit.text('kale, spinach and rocket smoothie')
-streamlit.text('hard_boiled free_range eggs')
+streamlit.text('🥣 omega 3 and blueberry oatmeal')
+streamlit.text('🥗 kale, spinach and rocket smoothie')
+streamlit.text('🐔 hard_boiled free_range eggs')
+streamlit.text('🥑🍞 avocado bread')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
