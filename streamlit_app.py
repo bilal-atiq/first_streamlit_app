f538387dd1f150ee518145b029b50fea9d091181
bilal-atiq <127108568+bilal-atiq@users.noreply.github.com>
2023-04-11 15:25:27 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(my_fruit_list)
 
 #create function
-def get fruityvice_data(this_fruit_choice):
+def get_fruityvice_data(this_fruit_choice):
 #Can You Add A Second Text Entry Box? streamlitimport requests
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
 #streamlit.text(fruityvice_response.json())
