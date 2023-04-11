e748b3e70b0b6d096b6ae5f8c65f3c1418d7307b
bilal-atiq <127108568+bilal-atiq@users.noreply.github.com>
2023-04-11 14:54:43 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-#from urllib.error import URLERROR
+from urllib.error import URLERROR
 
 streamlit.title('my parents new healthy diner')
 
@@ -26,8 +26,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(my_fruit_list)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  #streamlit.write('The user entered ', fruit_choice)
+  if not fruit_choice:
+    streamlit.error("please select a fruit to get information")
+  else:
+
 
 #Can You Add A Second Text Entry Box? streamlitimport requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
@@ -36,6 +41,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+  streamlit.error()
 
 add_my_fruit = streamlit.text_input('enter seconf fruit')
 
