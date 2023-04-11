0c18785d2cb721f2153ee96e83e92280eb705d5e
bilal-atiq <127108568+bilal-atiq@users.noreply.github.com>
2023-04-11 14:33:36 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,13 +29,15 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+#Can You Add A Second Text Entry Box? streamlitimport requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+add_my_fruit = streamlit.text_input("enter seconf fruit", add_my_fruit)
 
 streamlit.stop()
 
